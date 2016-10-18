8b046e85612b6cc1d9346537abe4ba6f3cc2121e
Bruno STEVANT <bruno.stevant@telecom-bretagne.eu>
2016-10-18 16:54:12 +0200

    webui: reload gallery after xhr

--- webui/views/dropzone.js
+++ webui/views/dropzone.js
@@ -3,7 +3,15 @@ Dropzone.options.photodropzone = {
       this.on("complete", function(file) {
           this.removeFile(file);
           //history.go(0);
-		  $("#NG1").load(window.location.href + "photos" );
+		  $("#NG1").load(window.location.href + "photos", function() {
+		      $("#NG1").nanoGallery({
+		          //itemsBaseURL:'http://localhost:3000',
+		          thumbnailWidth: 'auto',
+		          thumbnailHeight: 100,
+		          locationHash: false,
+		          thumbnailHoverEffect:'borderLighter,imageScaleIn80'
+		      });
+		  });
       });
   }
 };
