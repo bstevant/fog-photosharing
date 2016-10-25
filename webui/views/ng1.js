ba90f1429acc89ab0d9b78cf390c904bc6852b22
Bruno STEVANT <bruno.stevant@telecom-bretagne.eu>
2016-10-25 15:14:49 +0200

    Webui: changed gallery theme

--- webui/views/ng1.js
+++ webui/views/ng1.js
@@ -1,11 +1,12 @@
 $(document).ready(function () {
-    $("#NG1").nanoGallery({
-        //itemsBaseURL:'http://localhost:3000',
-        thumbnailWidth: 'auto',
-        thumbnailHeight: 100,
-        locationHash: false,
-        thumbnailHoverEffect:'borderLighter,imageScaleIn80',
+	$("#NG1").nanoGallery({
+		//itemsBaseURL:'http://localhost:3000',
+		theme:'clean'
+		thumbnailWidth: 'auto',
+		thumbnailHeight: 100,
+		locationHash: false,
+		thumbnailHoverEffect:'borderLighter,imageScaleIn80',
 		kind: 'json',
-        jsonProvider: './nanoPhotosProvider.php',
-    });
+		jsonProvider: './nanoPhotosProvider.php',
+	});
 });
