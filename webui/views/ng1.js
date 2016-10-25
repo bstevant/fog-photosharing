815603a5d05fec0d711963370090a074ec935803
Bruno STEVANT <bruno.stevant@telecom-bretagne.eu>
2016-10-25 16:47:23 +0200

    Webui: Pb with auto thumbnail size ?

--- webui/views/ng1.js
+++ webui/views/ng1.js
@@ -2,8 +2,8 @@ $(document).ready(function () {
 	$("#NG1").nanoGallery({
 		//itemsBaseURL:'http://localhost:3000',
 		theme:'clean',
-		thumbnailWidth: 'auto',
-		thumbnailHeight: 100,
+		thumbnailWidth: 150,
+		thumbnailHeight: 150,
 		locationHash: false,
 		thumbnailHoverEffect:'borderLighter,imageScaleIn80',
 		kind: 'json',
