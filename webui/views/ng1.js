12242ab6ecf3290361a06d4a8d25285a42934318
Bruno STEVANT <bruno.stevant@telecom-bretagne.eu>
2016-10-25 16:48:47 +0200

    Webui: remove clean theme

--- webui/views/ng1.js
+++ webui/views/ng1.js
@@ -1,7 +1,6 @@
 $(document).ready(function () {
 	$("#NG1").nanoGallery({
 		//itemsBaseURL:'http://localhost:3000',
-		theme:'clean',
 		thumbnailWidth: 150,
 		thumbnailHeight: 150,
 		locationHash: false,
